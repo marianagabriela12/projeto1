aa14a107b3c61bcae0f787f79789733ac55858e5
marianagabriela12 <105889661+marianagabriela12@users.noreply.github.com>
2024-04-22 19:38:35 -0300

    gijru9eh

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2032-11-20T23:59:59");
 const tempoObjetivo2 = new Date("2025-12-31T23:59:59");
-
+const tempo0bjetivo3 = new Date("2032-12-31T23:59:59");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
