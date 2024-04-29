0fa3e6ca2d6a2ef82cf99fe57174a84e5a383b9c
marianagabriela12 <105889661+marianagabriela12@users.noreply.github.com>
2024-04-29 19:32:07 -0300

    csmfpmoigv

--- main.js
+++ main.js
@@ -19,7 +19,9 @@ const tempoObjetivo1 = new Date("2032-11-20T23:59:59");
 const tempoObjetivo2 = new Date("2025-12-30T23:59:59");
 const tempo0bjetivo3 = new Date("2034-12-31T23:59:59");
 const tempoObjetivo4 = new Date("2036-10-08T23:59:59");
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -32,6 +34,27 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
+    } else {
+        return [0,0,0,0];
+    }
 }
+
+function atualizaCronometro(){
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+
+    for (let i=0; i<contadores.length;i++){
+       // contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
